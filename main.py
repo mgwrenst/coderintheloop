48e40d5239dc2b5aa736cbd2867811197c1db36c
mgwrenst <wren98sted@gmail.com>
2025-09-22 22:42:59 +0200

    #Draft for psycopg adapter

---
+++ main.py
@@ -0,0 +1,20 @@
+import psycopg
+
+conn = psycopg.connect(host="localhost", dbname="politikk", user="postgres", port=5432, password="password")
+
+
+cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS person (
+id SERIAL PRIMARY KEY,
+name VARCHAR(255), 
+age INT,
+gender CHAR 
+);
+""")
+
+cur.execute("""INSERT INTO person (id, name, age, gender) VALUES
+(1, 'Alice', 30, 'F'), 
+(2, 'Bob', 25, 'M'), 
+(3, 'Charlie', 35, 'M');
+""")
+
